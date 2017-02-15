d89df5425f12fa99839a6195b86d2f474b42d4b9
mfarid <mohd.farid@devfactory.com>
2017-02-15 11:03:55 +0530

    Updated views for mysql

--- src/main/java/guru/springframework/SpringBootMySqlApplication.java
+++ src/main/java/guru/springframework/SpringBootMySqlApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootMongodbApplication {
+public class SpringBootMySqlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMongodbApplication.class, args);
+		SpringApplication.run(SpringBootMySqlApplication.class, args);
 	}
 }
--- src/test/java/guru/springframework/SpringBootMySqlApplicationTests.java
+++ src/test/java/guru/springframework/SpringBootMySqlApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootMongodbApplicationTests {
+public class SpringBootMySqlApplicationTests {
 
 	@Test
 	public void contextLoads() {
